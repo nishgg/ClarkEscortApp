0b89368073bede1987292fe062910bc3777ab9df
Jocelyn Ng <jocelynng00@hotmail.com>
2019-04-21 16:09:40 -0400

    21/04/19: added transition from welcome page to main pages, added tabs to scroll through different pages

---
+++ app/src/main/java/com/example/escortapp2/FragMap.java
@@ -0,0 +1,24 @@
+package com.example.escortapp2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragMap extends Fragment {
+    View view;
+
+    public FragMap() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.map_frag, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/escortapp2/FragWaitTimes.java
@@ -0,0 +1,24 @@
+package com.example.escortapp2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragWaitTimes extends Fragment {
+    View view;
+
+    public FragWaitTimes() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.waittime_frag, container, false);
+
+        return view;
+    }
+}
